92ba625819f640db988b98d76ad4a9211494e48c
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2023-11-07 19:03:46 +0700

    update cn-da20tta-ngotanloi-ITshop-laravel

--- public/adminPublic/user/add/add.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.select2_init').select2({
-        'placeholder': 'Chọn vai trò'
-    })
-});
-    
-
-
---
+++ scr/app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+    protected $guarded=[];
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class,'product_id');
+    }
+    public function tags(){
+        return $this->belongsToMany(Tag::class, 'product_tags', 
+       'product_id', 'tag_id')-> withTimestamps();
+
+    }
+    public function category(){
+        return $this ->belongsTo( Category::class, 'category_id');
+    }
+    public function productImage(){
+        return $this->hasMany(ProductImage::class,  'product_id');
+    }
+}
